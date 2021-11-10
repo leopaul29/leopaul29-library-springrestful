4391629fe55b370345f41f431ee57bb8d27d8ff9
Leo-paul <leopaulmartin@hotmail.fr>
2021-11-10 23:16:30 +0100

    book controller

---
+++ src/main/java/com/leopaul29/library/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.leopaul29.library.controller;
+
+import com.leopaul29.library.exception.ResourceNotFoundException;
+import com.leopaul29.library.model.Book;
+import com.leopaul29.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // Get All books
+    @GetMapping("/books")
+    public List<Book> getAllNotes() {
+        return bookRepository.findAll();
+    }
+
+    // Create a new Note
+    @PostMapping("/notes")
+    public Book createNote(@Valid @RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Get a Single Note
+    @GetMapping("/notes/{id}")
+    public Book getNoteById(@PathVariable(value = "id") Long bookId) {
+        return bookRepository.findById(bookId)
+                .orElseThrow(() -> new ResourceNotFoundException("Book", "id", bookId));
+    }
+}
