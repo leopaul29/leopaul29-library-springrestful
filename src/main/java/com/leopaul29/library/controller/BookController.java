d1d4fc6d775833e78cceeb22d251faa284aa4fa2
Leo-paul <leopaulmartin@hotmail.fr>
2021-11-13 17:49:14 +0100

    spring boot restful

--- src/main/java/com/leopaul29/library/controller/BookController.java
+++ src/main/java/com/leopaul29/library/controller/BookController.java
@@ -4,6 +4,7 @@ import com.leopaul29.library.exception.ResourceNotFoundException;
 import com.leopaul29.library.model.Book;
 import com.leopaul29.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,15 +24,41 @@ public class BookController {
     }
 
     // Create a new Book
-    @PostMapping("/book")
+    @PostMapping("/books")
     public Book createBook(@Valid @RequestBody Book book) {
         return bookRepository.save(book);
     }
 
     // Get a Single Book
-    @GetMapping("/book/{id}")
+    @GetMapping("/books/{id}")
     public Book getBookById(@PathVariable(value = "id") Long bookId) {
         return bookRepository.findById(bookId)
                 .orElseThrow(() -> new ResourceNotFoundException("Book", "id", bookId));
+    }
+
+    // Update a Book
+    @PutMapping("/books/{id}")
+    public Book updateBook(@PathVariable(value = "id") Long bookId,
+                           @Valid @RequestBody Book bookDetails) {
+
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new ResourceNotFoundException("Book", "id", bookId));
+
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+
+        Book updatedBook = bookRepository.save(book);
+        return updatedBook;
+    }
+
+    // Delete a Book
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable(value = "id") Long bookId) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new ResourceNotFoundException("Book", "id", bookId));
+
+        bookRepository.delete(book);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/leopaul29/library/model/Book.java
+++ src/main/java/com/leopaul29/library/model/Book.java
@@ -1,7 +1,10 @@
 package com.leopaul29.library.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -9,6 +12,10 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity(name = "Book")
+@Table(name = "books")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
+        allowGetters = true)
 @Data
 public class Book implements Serializable {
     @Id
@@ -20,9 +27,14 @@ public class Book implements Serializable {
 
     @NotBlank
     private String author;
-/*
+
     @Column(nullable = false, updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     @CreatedDate
-    private Date createdAt;*/
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
 }
